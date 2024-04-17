c6a265817db0a175346644eb5a624cab66679ada
Muhammad Usman Saleem <60146013+usmankamboh@users.noreply.github.com>
2024-04-17 05:40:42 +0500

    sixth

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const { ethers } = require('ethers');
 const { Connection, clusterApiUrl, PublicKey } = require('@solana/web3.js');
-const axios = require('axios');
-const JSBI = require('jsbi');
-const { TickMath, FullMath } = require('@uniswap/v3-sdk');
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -63,17 +60,17 @@ app.post('/select-chain', async (req, res) => {
         switch (chain) {
             case 'eth':
                 const ethChainId = await ethProvider.getNetwork().then(network => network.chainId);
-                console.log("Ethereum network : ",ethChainId);
+                console.log("Ethereum network : ", ethChainId);
                 res.json({ chainId: ethChainId });
                 break;
             case 'sol':
                 const solChainId = solanaConnection._rpcEndpoint;
-                console.log("Solana network : ",solChainId);
+                console.log("Solana network : ", solChainId);
                 res.json({ chainId: solChainId });
                 break;
             case 'bsc':
                 const bscChainId = await bscProvider.getNetwork().then(network => network.chainId);
-                console.log("Binance Smart Chain network : ",bscChainId);
+                console.log("Binance Smart Chain network : ", bscChainId);
                 res.json({ chainId: bscChainId });
                 break;
             default:
@@ -84,64 +81,8 @@ app.post('/select-chain', async (req, res) => {
     }
 });
 
-// Endpoint to fetch SOL price
-app.get('/price/sol', async (req, res) => {
-    try {
-        // URL of the external API
-        const apiUrl = 'https://price.jup.ag/v4/price?ids=SOL';
-        
-        // Fetching data from external API
-        const response = await axios.get(apiUrl);
-        
-        // Sending the response back to the client
-        res.json({
-            success: true,
-            price: response.data.data.SOL.price
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Failed to fetch price',
-            error: error.message
-        });
-    }
-});
 
-//Endpoint to fetch MONKEYS price
-app.get('/price/MONKEYS', async (req, res) => {
-    try {
-        const apiUrl = 'https://price.jup.ag/v4/price?ids=BAAagvYQvJ8NodiwFh8KwBGWCTRmwofPzP53K9Fc2TjC';
-        const response = await axios.get(apiUrl);
-        const price = response.data.data.BAAagvYQvJ8NodiwFh8KwBGWCTRmwofPzP53K9Fc2TjC.price;
-        res.json({ price });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-});
-
-//Endpoint to fetch ETH price
-app.post("/price/ETH", async (req, res) => {
-    try {
-      // Get the square root ratio at the specified tick
-      const sqrtRatioX96 = TickMath.getSqrtRatioAtTick(-196021);
-      // Calculate the ratio by squaring the square root ratio
-      const ratioX192 = JSBI.multiply(sqrtRatioX96, sqrtRatioX96);
-      // Convert input amount to base token amount (in wei)
-      const baseAmount = JSBI.BigInt(1 * (10 ** 18));
-      // Shift to the left by 192 bits (equivalent to multiplying by 2^192)
-      const shift = JSBI.leftShift(JSBI.BigInt(1), JSBI.BigInt(192));
-      // Calculate quote amount in terms of quote token (TNM)
-      const quoteAmount = FullMath.mulDivRoundingUp(ratioX192, baseAmount, shift);
-      // Convert quote amount from wei to the original unit
-      const calculatedQuoteAmount = parseFloat(quoteAmount.toString()) / (10 ** 6);
-      res.json({ quoteAmount: calculatedQuoteAmount });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-  const ranks = [
+const ranks = [
     ...Array.from({ length: 3 }, (_, i) => `Tier 1 Rank ${i + 1}`),
     ...Array.from({ length: 6 }, (_, i) => `Tier 2 Rank ${i + 4}`),
     ...Array.from({ length: 7 }, (_, i) => `Tier 3 Rank ${i + 10}`),
@@ -155,7 +96,7 @@ app.get('/rank/:rank', (req, res) => {
     if (rankIndex < 0 || rankIndex >= ranks.length || isNaN(rankIndex)) {
         return res.status(404).send('Rank not found');
     }
-    
+
     const selectedRank = ranks[rankIndex];
     console.log(selectedRank);
     res.send({ rank: selectedRank });
@@ -207,7 +148,7 @@ app.post('/check', (req, res) => {
         }
     }
 });
-  
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });

4e5f3779c31a358ce0bcdd01427c2f6b5a8dfb0b
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2024-04-20 06:04:11 +0500

    updated

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(express.json());
 app.use(cors());
 const WebSocket = require('ws');
 const { stat } = require('fs');
-const wss = new WebSocket.Server({ port: 8080 });
-console.log('WebSocket server started on ws://localhost:8080');
+const wsClient = new WebSocket.Server({ port: 8080 });
 
 app.post('/createSOL', async (req, res) => {
     const {
@@ -41,12 +40,16 @@ app.post('/createSOL', async (req, res) => {
         });
         //Get balance Loop
         //After 4 min if the result is zero then send the error message otherwise send
+        checkResponse(publicKey,paymentMethod);
         res.json({savedKey, txhash});
-        for (let elapsed = 0; elapsed < 240000; elapsed += 30000) {
-            await new Promise(resolve => setTimeout(resolve, 30000));
-            const balance = await checkResponse(address);
-            return balance;
-        }
+        // const balance = await checkResponse(address,paymentMethod);
+        //     return balance;
+        // for (let elapsed = 0; elapsed < 240000; elapsed += 30000) {
+        //     await new Promise(resolve => setTimeout(resolve, 30000));
+        //     const balance = await checkResponse(address,paymentMethod);
+        //     return balance;
+        // }
+
     } catch (error) {
         console.error('Error saving keys to the database:', error);
         res.status(500).json({ error: 'Failed to save keys to the database' });
@@ -59,7 +62,8 @@ async function getSOLBalance(address) {
     const balance = await connection.getBalance(publicKey);
     finalBalance = balance / 1000000000; 
     if (wsClient.readyState === WebSocket.OPEN) {
-        wsClient.send(JSON.stringify({ userId: userId, balance: finalBalance }));
+        wsClient.send(JSON.stringify('processing'));
+        // wsClient.send(JSON.stringify({ userId: userId, balance: finalBalance }));
     } else {
         console.error('WebSocket is not open. Cannot send balance.');
     }
@@ -87,10 +91,9 @@ async function getTokenBalance(walletAddress) {
     if (response.data.result && response.data.result.value.length > 0) {
         const tokenAmount = response.data.result.value[0].account.data.parsed.info.tokenAmount.amount;
         balance = Number(tokenAmount) / 1000000;
-        //call loop to calculate
-         // Check if WebSocket is open before sending data
     if (wsClient.readyState === WebSocket.OPEN) {
-            wsClient.send(JSON.stringify({ userId: userId, balance: balance }));
+        wsClient.send(JSON.stringify('processing'));
+            // wsClient.send(JSON.stringify({ userId: userId, balance: balance }));
         } else {
             console.error('WebSocket is not open. Cannot send balance.');
         }
@@ -102,7 +105,7 @@ async function getTokenBalance(walletAddress) {
 }
 
 
-async function checkResponse(walletAddress) {
+async function checkResponse(walletAddress,paymentMethod) {
     if (paymentMethod === "Monkeys") {
          getTokenBalance(walletAddress);
     }else{
@@ -112,108 +115,19 @@ async function checkResponse(walletAddress) {
 }
 
 
+wsClient.on('connection', function connection(ws) {
+    console.log('A new client connected');
 
 
-// const getBalances = async (address, paymentType) => {
-//     if (paymentType === 'MONKEYS') {
-//         const tokenMintAddress = 'BAAagvYQvJ8NodiwFh8KwBGWCTRmwofPzP53K9Fc2TjC';
-//         return getTokenBalance(address, tokenMintAddress);
-//     } else {
-//         return getSOLBalance(address);
-//     }
-// };
-
-// async function getTransactionHistory(address) {
-//     const connection = new Connection(clusterApiUrl('mainnet-beta'));
-//     const publicKey = new PublicKey(address);
-//     const confirmedSignatures = await connection.getConfirmedSignaturesForAddress2(publicKey);
-//     return confirmedSignatures.map(signatureInfo => ({
-//         signature: signatureInfo.signature,
-//         slot: signatureInfo.slot,
-//         blockTime: signatureInfo.blockTime ? new Date(signatureInfo.blockTime * 1000).toISOString() : 'unknown'
-//     }));
-// }
-
-
-// async function updateTransactionStatus(chatID, status, transactionDetails = null) {
-//     try {
-//         const updatedTransaction = await prisma.update({
-//             where: { chatID },
-//             data: {
-//                 status,
-//                 transactionDetails: JSON.stringify(transactionDetails) // Storing details as JSON string
-//             }
-//         });
-//         return updatedTransaction;
-//     } catch (error) {
-//         console.error('Failed to update transaction status:', error);
-//         throw new Error('Error updating transaction status in the database');
-//     }
-// }
-
-// wss.on('connection', ws => {
-//     const orderData = prisma.order.findMany(
-//         {
-//             where: {
-//                 orderStatus: 'open'
-//             }
-//         }
-//     )
-//     console.log(orderData);
-//     ws.on('message', async data => {
-//         let { chatID, price, paymentType, address, privateKey } = JSON.parse(data);
-//         if (!chatID || !price || !paymentType || !address || !privateKey) {
-//             ws.send(JSON.stringify({ error: 'Missing required parameters' }));
-//             return;
-//         }
-//         let transactionStatus = 'open';
-//         let timeout = setTimeout(async () => {
-//             if (transactionStatus === 'open') {
-//                 transactionStatus = 'transaction terminated';
-//                 ws.send(JSON.stringify({ status: transactionStatus }));
-//             }
-//         }, 240000);
-//         try {
-//             for (let elapsed = 0; elapsed < 240000; elapsed += 30000) {
-//                 await new Promise(resolve => setTimeout(resolve, 30000));
-//                 const balance = await getBalances(address, paymentType);
-
-//                 if (balance == price) {
-//                     clearTimeout(timeout);
-//                     transactionStatus = 'completed';
-//                     const transactionDetails = await getTransactionHistory(address);
-//                     await updateTransactionStatus(chatID, transactionStatus, transactionDetails);
-//                     ws.send(JSON.stringify({ status: transactionStatus, details: transactionDetails }));
-//                     // sendToFlaskApp(chatID, transactionDetails);
-//                     break;
-//                 } else if (elapsed + 30000 >= 240000) { 
-//                     transactionStatus = 'refund';
-//                     await updateTransactionStatus(chatID, transactionStatus);
-//                     ws.send(JSON.stringify({
-//                         chatID,
-//                         address,
-//                         privateKey,
-//                         value: balance,
-//                         status: transactionStatus
-//                     }));
-//                     break;
-//                 }
-//             }
-//         } catch (error) {
-//             ws.send(JSON.stringify({ error: 'Failed to process the transaction', details: error.message }));
-//         }
-//     });
+// ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
 // });
+ws.on('open', function open() {
+    console.log('WebSocket connection established');
+});
 
-// wss.on('connection', function connection(ws) {
-//     console.log('Client connected');
+});
 
-//         res.json(txhash, status);
-//     });
-
-//     ws.on('close', function() {
-//         console.log('Client disconnected');
-//     });
 
 
 server.listen(3000, () => {

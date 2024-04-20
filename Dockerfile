e04219d766dfac61155f2b04c89d76884a14c01c
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2024-04-20 15:22:00 +0500

    latest push

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# Step 1: Choose the base Image
+FROM node:20
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+COPY prisma ./prisma
+RUN npx prisma generate
+EXPOSE 8080
+CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -307,6 +307,8 @@ app.get('/', async (req, res) => {
 });
 
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
